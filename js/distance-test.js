8c41c42eccf79bd399a7c9e9b29544de4df70a17
James Mosier <jamesdmosier@gmail.com>
2013-09-05 22:12:01 -0400

    For loop is still not displaying all the results for distance-test.js

--- js/distance-test.js
+++ js/distance-test.js
@@ -1,160 +1,150 @@
-var map;
+var trailfinder_map;
 
-function initialize() {
-	var mapOptions = {
-		zoom: 9,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-	// Try HTML5 geolocation
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			var lat = position.coords.latitude,
-				lng = position.coords.longitude,
-				//lat=41.081445,lng= -81.519005,
-				pos = new google.maps.LatLng(lat, lng),
-				//query from FusionTables for the map
-				map_query = {
-					select: 'Location',
-					from: '1MsmdOvWLKNNrtKnmoEf2djCc3Rp_gYmueN4FGnc',
-					limit: 3,
-					orderBy: 'ST_DISTANCE(Coordinates, LATLNG(' + lat + ',' + lng + '))'
-				},
-				ftLayer = new google.maps.FusionTablesLayer({
-					map: map,
-					query: map_query
-				}),
-				geolocMarker = new google.maps.Marker({
-					map: map,
-					position: pos,
-					clickable: true,
-					icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png'
-				});
-			geolocMarker.info = new google.maps.InfoWindow({
-				content: '<span style="font-weight: bold;" class="users-location-marker">Your location</span>'
-			});
-			google.maps.event.addListener(geolocMarker, 'click', function() {
-				geolocMarker.info.open(map, geolocMarker);
-			});
-			map.setCenter(pos);
-			//query for the table
-			var listQuery = "SELECT Name, Coordinates FROM " + '1MsmdOvWLKNNrtKnmoEf2djCc3Rp_gYmueN4FGnc' + ' ORDER BY ST_DISTANCE(Coordinates, LATLNG(' + lat + ',' + lng + '))' + ' LIMIT 3';
-			var encodedQuery = encodeURIComponent(listQuery);
-			// Construct the URL
-			var url = ['https://www.googleapis.com/fusiontables/v1/query'];
-			url.push('?sql=' + encodedQuery);
-			url.push('&key=AIzaSyAJ_2Gtxlr4jeFuBup_jyRa5taZGk20JLs');
-			url.push('&callback=?');
-			// Send the JSONP request using jQuery
-			$.ajax({
-				url: url.join(''),
-				dataType: 'jsonp',
-				success: function(data) {
-					var rows = data['rows'];
-					var ftData = document.getElementById('sidebar-data');
-					for (var i in rows) {
-						var name = rows[i][0];
-						var sidebarCoordinates = rows[i][1];
-						
-						//var distanceCell = rows[i][2];
-						
-						var dataElement = document.createElement('tr');
-						var nameElement = document.createElement('td');
-						nameElement.innerHTML = name;
-						nameElement.className = 'name-name';
-						var coordinatesElement = document.createElement('td');
-						coordinatesElement.innerHTML = sidebarCoordinates;
-						coordinatesElement.className = 'coordinates';
-						
-						//var distanceElement = document.createElement('td')
-						//distanceElement.innerHTML = distanceCell;
-						//distanceElement.className = 'distanceTD';
-						
-						dataElement.appendChild(nameElement);
-						dataElement.appendChild(coordinatesElement);
-						//dataElement.appendChild(distanceElement);
-						ftData.appendChild(dataElement);
-					}
-						
+function trailfinder_initialize() {
+    var mapOptions = {
+        zoom: 9,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    trailfinder_map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var lat = position.coords.latitude,
+                lng = position.coords.longitude,
+                //lat=41.081445,lng= -81.519005,
+                pos = new google.maps.LatLng(lat, lng),
+                //query from FusionTables for the map
+                map_query = {
+                    select: 'Location',
+                    from: '1MsmdOvWLKNNrtKnmoEf2djCc3Rp_gYmueN4FGnc',
+                    limit: 3,
+                    orderBy: 'ST_DISTANCE(Coordinates, LATLNG(' + lat + ',' + lng + '))'
+                },
+                ftLayer = new google.maps.FusionTablesLayer({
+                    map: trailfinder_map,
+                    query: map_query
+                }),
+                geolocMarker = new google.maps.Marker({
+                    map: trailfinder_map,
+                    position: pos,
+                    clickable: true,
+                    icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png'
+                });
+            geolocMarker.info = new google.maps.InfoWindow({
+                content: '<span style="font-weight: bold;" class="users-location-marker">Your location</span>'
+            });
+            google.maps.event.addListener(geolocMarker, 'click', function () {
+                geolocMarker.info.open(map, geolocMarker);
+            });
+            trailfinder_map.setCenter(pos);
+            //query for the table
+            var listQuery = "SELECT Name, Coordinates FROM " 
+            + '1MsmdOvWLKNNrtKnmoEf2djCc3Rp_gYmueN4FGnc' 
+            + ' ORDER BY ST_DISTANCE(Coordinates, LATLNG(' + lat + ',' + lng + '))' + ' LIMIT 3';
+            var encodedQuery = encodeURIComponent(listQuery);
+            // Construct the URL
+            var url = ['https://www.googleapis.com/fusiontables/v1/query'];
+            url.push('?sql=' + encodedQuery);
+            url.push('&key=AIzaSyAJ_2Gtxlr4jeFuBup_jyRa5taZGk20JLs');
+            url.push('&callback=?');
+            // Send the JSONP request using jQuery
+            $.ajax({
+                url: url.join(''),
+                dataType: 'jsonp',
+                success: function (data) {
+                    var rows = data['rows'];
+                    var ftData = document.getElementById('sidebar-data');
+                    for (var i in rows) {
+                        var name = rows[i][0];
+                        var sidebarCoordinates = rows[i][1];
+                        var dataElement = document.createElement('tr');
+                        var nameElement = document.createElement('td');
+                        nameElement.innerHTML = name;
+                        nameElement.className = 'name-name';
+                        var coordinatesElement = document.createElement('td');
+                        coordinatesElement.innerHTML = sidebarCoordinates;
+                        coordinatesElement.className = 'coordinates';
+                        dataElement.appendChild(nameElement);
+                        dataElement.appendChild(coordinatesElement);
+                        ftData.appendChild(dataElement);
+                    }
 
+                    var origins = pos,
+					    destinations = sidebarCoordinates,
+					    service = new google.maps.DistanceMatrixService();
 
+                    service.getDistanceMatrix(
+					    {
+					        origins: [origins],
+					        destinations: [destinations],
+					        travelMode: google.maps.TravelMode.DRIVING,
+					        unitSystem: google.maps.UnitSystem.IMPERIAL,
+					        avoidHighways: false,
+					        avoidTolls: false
+					    },
+					    callback
+					);
 
-						var origins = pos,
-						    destinations = sidebarCoordinates,
-						    service = new google.maps.DistanceMatrixService();
-						
-						service.getDistanceMatrix(
-						    {
-						        origins: [origins],
-						        destinations: [destinations],
-						        travelMode: google.maps.TravelMode.DRIVING,
-						        avoidHighways: false,
-						        avoidTolls: false
-						    }, 
-						    callback
-						);
+                    function callback(response, status) {
+                        if (status == google.maps.DistanceMatrixStatus.OK) {
+                            var origins = response.originAddresses;
+                            var destinations = response.destinationAddresses;
+                            var outputDiv = document.getElementById('outputDiv');
+                            outputDiv.innerHTML = '';
 
-						function callback(response, status) {
-						  if (status == google.maps.DistanceMatrixStatus.OK) {
-						    var origins = response.originAddresses;
-						    var destinations = response.destinationAddresses;
-						    var outputDiv = document.getElementById('outputDiv');
-						    outputDiv.innerHTML = '';
+                            for (var k = 0; k < origins.length; k++) {
+                                console.log('first for loop..K to be exact');
+                                var results = response.rows[k].elements;
 
-						    for (var k = 0; k < origins.length; k++) {
-						      console.log('first for loop..K to be exact');
-						      var results = response.rows[k].elements;
-						      
-						      for (var j = 0; j < results.length; j++) {
-						      console.log('i am inside the var j loop.');
-						        var element = results[j];
-						        var distance = element.distance.text;
-						        var duration = element.duration.text;
-						        var from = origins[k];
-						        var to = destinations[j];
-						        
-						        console.log('the results are spitting out...');
-						        //this outputs the distance & time to the id="outputDiv" on the distance.html page
-						        //outputDiv.innerHTML += results[j].distance.text 
-						        //+ ' in '
-					            //+ results[j].duration.text + '<br>';
-						        
-						        $("#theresult").text(response.rows[0].elements[0].distance.text + " and " + response.rows[0].elements[0].duration.text);
- 
-						        
-						        
-						        //outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
-					            //+ ': ' + results[j].distance.text + ' in '
-					            //+ results[j].duration.text + '<br>';
-						      }
-						    }
-						  }
-						}
+                                for (var j = 0; j < results.length; j++) {
+                                    console.log('i am inside the var j loop.');
+                                    var element = results[j];
+                                    var distance = element.distance.text;
+                                    var duration = element.duration.text;
+                                    var from = origins[k];
+                                    var to = destinations[j];
 
-			}//ends if statement?
-			});//ends ajax call
-		}, function() {
-			handleNoGeolocation(true);
-		});
-} else {
-	// Browser doesn't support Geolocation
-	handleNoGeolocation(false);
-}
+                                    console.log('the results are spitting out...');
+                                    $("#theresult").text(response.rows[0].elements[0].distance.text + " and " + response.rows[0].elements[0].duration.text);
+
+                                    //different way of outputting results, as opposed to above
+                                    //outputDiv.innerHTML += results[j].distance.text 
+                                    //+ ' in '
+                                    //+ results[j].duration.text + '<br>';
+
+                                    //full output below
+                                    //outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
+                                    //+ ': ' + results[j].distance.text + ' in '
+                                    //+ results[j].duration.text + '<br>';
+                                }
+                            }
+                        }
+                    }
+
+                }
+            });
+        }, function () {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
 }
 //error handling for geolocation
 
 function handleNoGeolocation(errorFlag) {
-	if (errorFlag) {
-		var content = 'Error: The Geolocation service failed.';
-	} else {
-		var content = 'Error: Your browser doesn\'t support geolocation.';
-	}
-	var options = {
-		map: map,
-		position: new google.maps.LatLng(41.081445, -81.519005),
-		content: content
-	};
-	var infowindow = new google.maps.InfoWindow(options);
-	map.setCenter(options.position);
+    if (errorFlag) {
+        var content = 'Error: The Geolocation service failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(41.081445, -81.519005),
+        content: content
+    };
+    var infowindow = new google.maps.InfoWindow(options);
+    trailfinder_map.setCenter(options.position);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', trailfinder_initialize);
