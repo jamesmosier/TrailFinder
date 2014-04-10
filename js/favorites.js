b44eb4a0092e7789efeb329fb955ba3cd8f7392a
James Mosier <jamesdmosier@gmail.com>
2014-04-10 08:07:06 -0400

    added local storage for favs

---
+++ js/favorites.js
@@ -0,0 +1,20 @@
+
+
+
+
+// $( "[data-add='fav']" ).click(function() {
+// 	var favInput = $("#fav-value").text();
+// 	//var input = document.getElementById("saveServer");
+// 	localStorage.setItem("favorite", favInput);
+
+// });
+
+//$(document).ready(function(){
+	// var storedValue = localStorage.getItem("favorite");
+	// $(".favs-list ul li").html(storedValue);
+//});
+
+
+// $.fn.removeItem = function () {
+// 	localStorage.removeItem(key);
+// }
