d00f3cb6a3c21e9c5b05b7470c56c59dcbd47aae
James Mosier <jamesdmosier@gmail.com>
2014-04-14 11:59:09 -0400

    added angualr-seed,server.js, Bower, & angularJS

---
+++ app-info.md
@@ -0,0 +1,13 @@
+##To Start reload##
+cd to app folder
+type: reload -b
+
+(runs on localhost:8080)
+source: https://www.npmjs.org/package/reload
+
+#old way of doing it with my own server.js file#
+##To Start Server##
+npm start 
+
+##To End Server##
+CTRL-C 
--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(8080);
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , reload = require('reload')
+
+var app = express()
+
+var publicDir = path.join(__dirname, 'public')
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000)
+  app.use(express.logger('dev'))
+  app.use(express.bodyParser()) //parses json, multi-part (file), url-encoded
+  app.use(app.router) //need to be explicit, (automatically adds it if you forget)
+  app.use(express.static(clientDir)) //should cache static assets
+})
+
+app.get('/', function(req, res) {
+  res.sendfile(path.join(publicDir, 'index.html'))
+})
+
+var server = http.createServer(app)
+
+//reload code here
+reload(server, app)
+
+server.listen(app.get('port'), function(){
+  console.log("Web server listening on port " + app.get('port'));
+});
