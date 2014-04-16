34194aee02c2cefad40cfc0a122ed5476f9e1099
James Mosier <jamesdmosier@gmail.com>
2014-04-16 16:12:43 -0400

    added directive for tfinderMap plugin

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,15 @@
 
 
 angular.module('tFinder.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+  // directive('appVersion', ['version', function(version) {
+  //   return function(scope, elm, attrs) {
+  //     elm.text(version);
+  //   };
+  directive('tfinderMap', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            $(element).tfindermap(scope.$eval(attrs.tfinderMap));
+        }
     };
-  }]);
+  });
