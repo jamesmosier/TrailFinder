22f7a5d7e303f89c64b003b143f8112278f4a752
James Mosier <james.mosier@veritix.com>
2014-08-13 13:07:05 -0400

    began the refactor using onsen

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,59 +2,40 @@ module.exports = function(grunt) {
 // Load Grunt tasks declared in the package.json file
 require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-//GOT IT FROM HERE: http://thecrumb.com/2014/03/15/using-grunt-for-live-reload/
+//example taken from: http://thecrumb.com/2014/03/15/using-grunt-for-live-reload/
 grunt.initConfig({
+    
 
-    // Grunt express - our webserver
-    // https://github.com/blai/grunt-express
-    express: {
-        all: {
-            options: {
-                server: "server.js",
-                bases: ['C:\\Git\\TrailFinder\\app'],
-                //bases: ['Sites/Trailfinder'],
-                port: 8080,
-                hostname: "0.0.0.0",
-                livereload: true
-            }
-        }
-    },
+    //https://github.com/gruntjs/grunt-contrib-sass
     sass: {
         dist: {
             files: {
-                'app/css/app.css': 'app/sass/app.scss'
+                'www/styles/app.css': 'www/sass/app.scss'
             }
         }
     },
+
+    //https://github.com/gruntjs/grunt-contrib-cssmin
     cssmin: {
       combine: {
         files: {
-          'app/css/app.min.css': ['app/css/app.css']
+          'www/styles/app.min.css': ['www/styles/app.css']
         }
       }
     },
-    // grunt-watch will monitor the projects files
-    // https://github.com/gruntjs/grunt-contrib-watch
-    watch: {
-        all: {
-                files: ['app/*.html', 'app/sass/*.scss'],
-                tasks: ['sass', 'cssmin'],
-                options: {
-                    livereload: true
-            }
-        }
-    },
-    // grunt-open will open your browser at the project's URL
-    // https://www.npmjs.org/package/grunt-open
-    open: {
-        all: {
-            path: 'http://localhost:8080/index.html'
+
+    //combine files like this: 'assets/js/output.js': ['js/input.js', 'js/input2.js']
+    uglify: {
+        scriptz: {
+          files: {
+            'www/js/app.min.js': 'www/js/app.js'
+          }
         }
     }
+
     });
 
-    grunt.registerTask('build', ['sass', 'cssmin']);
-    grunt.registerTask('default', ['build', 'express', 'open', 'watch']);
+    grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
 
 };
     
